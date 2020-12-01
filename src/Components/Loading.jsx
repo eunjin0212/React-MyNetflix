78a297bad71cab6d38d992f6626290fabc2e71a9
eunjin0212 <eunjin9639@gmail.com>
2020-12-01 22:18:58 +0900

    Loadging

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,21 +1,8 @@
 import React from "react";
-import styled, { keyframes } from "styled-components";
+import ReactLoading from "react-loading";
 
-const rotateplane = keyframes`
-  0% { 
-    transform: perspective(120px) rotateX(0deg) rotateY(0deg);
-  } 50% { 
-    transform: perspective(120px) rotateX(-180.1deg) rotateY(0deg);
-  } 100% { 
-    transform: perspective(120px) rotateX(-180deg) rotateY(-179.9deg);
-  }
-`;
-const Box = styled.div`
-  width: 40px;
-  height: 40px;
-  background-color: #fff;
-  margin: 10% auto;
-  animation: ${rotateplane} 1.2s infinite ease-in-out;
-`;
+const Loading = ({ type, color }) => (
+  <ReactLoading type={"bubbles"} color={"pink"} height={667} width={375} />
+);
 
-export default () => <Box />;
+export default Loading;
