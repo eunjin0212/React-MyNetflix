210ec45b9fb8f68852760de21160bb32eb24905f
eunjin0212 <eunjin9639@gmail.com>
2020-12-01 22:29:56 +0900

    Loading css

--- src/Components/Loading.jsx
+++ src/Components/Loading.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import ReactLoading from "react-loading";
+import styled from "styled-components";
+
+const LoadingContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-item: center;
+  width: 100px;
+  height: 100px;
+  margin: 10% auto;
+`;
 
 const Loading = ({ type, color }) => (
-  <ReactLoading type={"bubbles"} color={"pink"} height={667} width={375} />
+  <LoadingContainer>
+    <ReactLoading type={"spin"} color={"pink"} height={40} width={40} />
+  </LoadingContainer>
 );
 
 export default Loading;
